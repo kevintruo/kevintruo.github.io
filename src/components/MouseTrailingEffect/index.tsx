0079fcc74dd7d4c2d354a6046121df38ffb1a413
Kevin Truong <kevintruong088@gmail.com>
2023-07-01 01:07:08 -0400

    Added more circles

--- src/components/MouseTrailingEffect/index.tsx
+++ src/components/MouseTrailingEffect/index.tsx
@@ -11,49 +11,45 @@ interface CircleProps {
 
 export default function MouseTrailingEffect() {
   const { xAxis, yAxis } = useMouseTracking();
-
-  const [circles, setCircles] = React.useState<CircleProps[]>([
-    { xAxis: 0, yAxis: 0, x: 0, y: 0, scale: 1 },
-    { xAxis: 0, yAxis: 0, x: 0, y: 0, scale: 1 },
-    { xAxis: 0, yAxis: 0, x: 0, y: 0, scale: 1 },
-    { xAxis: 0, yAxis: 0, x: 0, y: 0, scale: 1 },
-    { xAxis: 0, yAxis: 0, x: 0, y: 0, scale: 1 },
-    { xAxis: 0, yAxis: 0, x: 0, y: 0, scale: 1 },
-    { xAxis: 0, yAxis: 0, x: 0, y: 0, scale: 1 },
-    { xAxis: 0, yAxis: 0, x: 0, y: 0, scale: 1 },
-    { xAxis: 0, yAxis: 0, x: 0, y: 0, scale: 1 },
-    { xAxis: 0, yAxis: 0, x: 0, y: 0, scale: 1 },
-  ]);
+  let arr = [];
+  const LEN = 25;
+  for (let i = 0; i < LEN; i++) {
+    arr.push({ xAxis: 0, yAxis: 0, x: 0, y: 0, scale: 1 });
+  }
+  const [circles, setCircles] = React.useState<CircleProps[]>(arr);
 
   React.useEffect(() => {
-    setCircles((prevCircles) => {
-      let x = xAxis ?? 0;
-      let y = yAxis ?? 0;
-      prevCircles.forEach((circle, index) => {
-        circle.xAxis = x - 12;
-        circle.yAxis = y - 12;
-        circle.x = x;
-        circle.y = y;
-        circle.scale = (prevCircles.length - index) / prevCircles.length;
-        const nextCircle = prevCircles[index + 1] || prevCircles[0];
-        x += (nextCircle.x - x) * 0.3;
-        y += (nextCircle.y - y) * 0.3;
+    function animateCircles() {
+      setCircles((prevCircles) => {
+        let x = xAxis ?? 0;
+        let y = yAxis ?? 0;
+        prevCircles.forEach((circle, index) => {
+          circle.xAxis = x - 12;
+          circle.yAxis = y - 12;
+          circle.x = x;
+          circle.y = y;
+          circle.scale = (prevCircles.length - index) / prevCircles.length;
+          const nextCircle = prevCircles[index + 1] || prevCircles[0];
+          x += (nextCircle.x - x) * 0.1;
+          y += (nextCircle.y - y) * 0.1;
+        });
+        return [...prevCircles];
       });
-      return [...prevCircles];
-    });
+
+      requestAnimationFrame(animateCircles);
+    }
+
+    animateCircles();
   }, [xAxis, yAxis]);
 
   return (
-    <>
+    <div className='cursor' style={{ left: yAxis, top: xAxis }}>
       {circles.map((circle, index) => {
-        return (
-          xAxis &&
-          yAxis && (
-            <CircleDiv key={index} xAxis={circle.xAxis} yAxis={circle.yAxis} scale={circle.scale} />
-          )
+        return !xAxis || !yAxis ? null : (
+          <CircleDiv key={index} xAxis={circle.xAxis} yAxis={circle.yAxis} scale={circle.scale} />
         );
       })}
-    </>
+    </div>
   );
 }
 
@@ -65,13 +61,13 @@ interface CircleDivProps {
 
 const CircleDiv = ({ xAxis, yAxis, scale }: CircleDivProps) => {
   return (
-    <>
-      {xAxis && yAxis && scale && (
-        <div
-          className={`w-8 h-8 rounded-full bg-black dark:bg-white absolute scale-[${scale}]`}
-          style={{ top: yAxis, left: xAxis, transform: `scale(${scale})` }}
-        />
-      )}
-    </>
+    <div
+      className={`w-8 h-8 rounded-full bg-black dark:bg-white absolute scale-[${scale}]`}
+      style={{
+        top: yAxis,
+        left: xAxis,
+        transform: `scale(${scale})`,
+      }}
+    />
   );
 };
