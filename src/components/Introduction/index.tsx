56f55e4c9950cd78f412dcb1e85e3962db344bd5
kevintruo <kevintruong088@gmail.com>
2023-09-04 21:49:36 -0400

    Updated landing page and extracted divs into panels

---
+++ src/components/Introduction/index.tsx
@@ -0,0 +1,11 @@
+import ElementWrapper from "../ElementWrapper";
+
+export default function Introduction() {
+  return (
+    <ElementWrapper>
+      <div className='bg-white text-black dark:bg-black dark:text-white'>
+        <h1>Hello World</h1>
+      </div>
+    </ElementWrapper>
+  );
+}
