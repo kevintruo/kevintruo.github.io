a33a70150c18da611bf2fa60627a91ec79e733e2
kevintruo <ktruo@kevintruo.com>
2023-09-10 21:28:28 -0400

    Updated landing page and extracted divs into panels

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,22 @@
-import AnimatedTypography from "./components/AnimatedTypography";
-import EyeGlassesLogo from "./components/EyeGlassesLogo";
+import LandingPage from "./components/LandingPage";
 import MouseTrailingEffect from "./components/MouseTrailingEffect";
-import Socials from "./components/Socials";
-import DownArrow from "./static/svg/DownArrow";
+import LeftPanel from "./components/Panels/LeftPanel";
+import RightPanel from "./components/Panels/RightPanel";
 
 function App() {
   const supportsTouch = "ontouchstart" in window || navigator.maxTouchPoints > 0;
 
   return (
-    <div className='flex h-screen flex-row bg-white text-black dark:bg-black dark:text-white'>
-      {!supportsTouch && (
-        <div className='h-screen w-64 border-black dark:border-white flex flex-col items-center justify-end'>
-          <Socials />
-          <div className='flex-grow max-h-[100px]' />
+    <div className='flex h-screen w-screen flex-row bg-white text-black dark:bg-black dark:text-white'>
+      {!supportsTouch && <LeftPanel />}
+      <div className='grow flex flex-row'>
+        <div className='flex-none w-64 h-screen' />
+        <div className='flex flex-col w-full'>
+          <LandingPage />
         </div>
-      )}
-      <div className='flex h-screen w-screen flex-col items-center justify-center'>
-        <div className='flex-grow' />
-        <EyeGlassesLogo />
-        <div className='animate-fade'>
-          <AnimatedTypography
-            textArr={["WELCOME"]}
-            className='text-3xl font-bold text-black  dark:text-white dark:bg-black'
-            repeat={true}
-          />
-        </div>
-        <div className='flex-grow' />
-        <div className='rounded-full animate-bounce mb-5 flex flex-col items-center'>
-          <DownArrow />
-          <p>Start scrolling</p>
-        </div>
+        <div className='flex-none w-64 h-screen' />
       </div>
-      {!supportsTouch && <div className='h-screen w-64 border-black dark:border-white' />}
+      {!supportsTouch && <RightPanel />}
       {!supportsTouch && <MouseTrailingEffect />}
     </div>
   );
