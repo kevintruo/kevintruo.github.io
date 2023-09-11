aea58aefee556efb02e3f2bab19768027715231a
kevintruo <ktruo@kevintruo.com>
2023-09-10 21:30:14 -0400

    Added typing animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { TypeAnimation } from "react-type-animation";
+
 function App() {
   return (
     <div className='flex h-screen'>
-      <h1 className='text-3xl font-bold m-auto'>COMING SOON ...</h1>
+      <TypeAnimation
+        sequence={["COMING SOON ...", 5000, "SERIOUSLY, IT'S COMING ...", 20000]}
+        wrapper='span'
+        speed={1}
+        className='text-3xl font-bold m-auto'
+      />
     </div>
   );
 }
