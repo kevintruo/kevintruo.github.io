5417d47b0135579aa6d0a153a6d70edc4e9cc573
kevintruo <ktruo@kevintruo.com>
2023-09-10 21:28:23 -0400

    Added useMouseTracking to get mouse coordinates

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import ThemeProvider from "./hooks/useTheme/Provider";
+import MouseTrackingProvider from "./hooks/useMouseTracking/Provider";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <MouseTrackingProvider>
+        <App />
+      </MouseTrackingProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
