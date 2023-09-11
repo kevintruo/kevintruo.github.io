c7371db2ad5ced1d380dc95f6eb267a0a008ae27
kevintruo <ktruo@kevintruo.com>
2023-09-10 21:28:24 -0400

    Recreated react-type-animation alike component

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,9 @@ function App() {
   return (
     <div className='flex h-screen flex-col dark:bg-black dark:text-white'>
       <AnimatedTypography
-        textArr={["COMING SOON..."]}
+        textArr={["COMING SOON ...", "SERIOUSLY, IT'S COMING ..."]}
         className='text-3xl font-bold m-auto text-black dark:text-white'
+        repeat={true}
       />
       <MouseTrailingEffect />
     </div>
--- src/components/AnimatedTypography/index.tsx
+++ src/components/AnimatedTypography/index.tsx
@@ -5,9 +5,18 @@ import { DefaultProps } from "../../models/defaultProps";
 
 interface Props extends DefaultProps {
   textArr: string[];
+  speed?: number;
+  betweenDelay?: number;
+  repeat?: boolean;
 }
 
-export default function AnimatedTypography({ textArr, className }: Props) {
+export default function AnimatedTypography({
+  textArr,
+  className,
+  speed = 100,
+  betweenDelay = 3000,
+  repeat = true,
+}: Props) {
   if (textArr.length < 1) {
     throw new Error("Array text must have at least one element");
   }
@@ -23,17 +32,34 @@ export default function AnimatedTypography({ textArr, className }: Props) {
 
     async function handleTypingAnimation() {
       setDisplay("");
-      for (let letter of textArr[0].split("")) {
-        await sleep(100, controller).then(() => {
-          setDisplay((s) => s + letter);
-        });
+      for (let [index, text] of textArr.entries()) {
+        const letters = text.split("");
+        for (let letter of letters) {
+          await sleep(speed, controller).then(() => {
+            setDisplay((s) => s + letter);
+          });
+        }
+
+        await sleep(betweenDelay, controller);
+
+        if (index === textArr.length - 1) {
+          return;
+        }
+
+        //TODO: Comeback to create
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        for (let _ in letters.reverse()) {
+          await sleep(speed, controller).then(() => {
+            setDisplay((s) => s.substring(0, s.length - 1));
+          });
+        }
       }
     }
 
     handleTypingAnimation();
 
     return () => controller.abort();
-  }, [textArr]);
+  }, [textArr, speed, betweenDelay]);
 
   return (
     <>
