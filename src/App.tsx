20fe849e5d2d4895c4f79bd1fc66d522f9358b29
kevintruo <ktruo@kevintruo.com>
2023-09-10 21:30:15 -0400

    Added theme toggle icon

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { TypeAnimation } from "react-type-animation";
 
 function App() {
   return (
-    <div className='flex h-screen'>
+    <div className='flex h-screen dark:bg-black dark:text-white'>
       <TypeAnimation
         sequence={["COMING SOON ...", 5000, "SERIOUSLY, IT'S COMING ...", 20000]}
         wrapper='span'
         speed={1}
-        className='text-3xl font-bold m-auto'
+        className='text-3xl font-bold m-auto dark:text-white'
       />
     </div>
   );
